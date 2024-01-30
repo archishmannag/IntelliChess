0c48f8a79720f375b8d49865b36a7a1f3be9bd0f
CruzerNag <50835593+archishmannag@users.noreply.github.com>
2024-01-31 01:26:29 +0530

    Added the initial board setup with the pieces in their correct positions. The pieces can be moved by other objects.

--- include/board.h
+++ include/board.h
@@ -4,15 +4,55 @@
 #include <vector>
 #include <cstdint>
 
-typedef union
+#ifndef PIECE_ENUM
+#define PIECE_ENUM
+
+typedef enum
 {
-	uint16_t piece_color;
-	struct
-	{
-		uint8_t piece : 6;
-		uint8_t isWhitePiece : 1;
-		uint8_t isHighlighted : 1;
-	};
+	pawn = 1,
+	rook = 2,
+	knight = 4,
+	bishop = 8,
+	queen = 16,
+	king = 32,
+	isWhitePiece = 64
+} pieceFlags;
+
+typedef enum
+{
+	isHighlighted = 128,
+	isOccupied = 256,
+	canPromote = 512,
+	canEnPassant = 1024,
+	canCastle = 2048,
+	hasMoved = 4096,
+	isInCheck = 8192,
+	isInCheckmate = 16384,
+	isInStalemate = 32768
+} otherFlags;
+
+#endif
+
+typedef struct
+{
+	uint16_t state;
 } tile_t;
+
+class Board
+{
+private:
+	int file = 5, rank = 5;
+	std::vector<std::vector<tile_t>> board;
+
+public:
+	Board();
+	~Board();
+	void initialize();
+	std::vector<std::vector<tile_t>> &getBoard();
+	int getRank();
+	int getFile();
+	void setRank(int rank);
+	void setFile(int file);
+};
 
 #endif
--- src/Board/board.cpp
+++ src/Board/board.cpp
@@ -1,28 +1,73 @@
 #include "../../include/board.h"
 
-class Board
+Board::Board()
 {
-private:
-	std::vector<std::vector<tile_t>> board;
-
-public:
-	Board()
+	// Initialize the board
+	board.resize(8);
+	for (int i = 0; i < 8; i++)
 	{
-		// Initialize the board
-		board.resize(8);
-		for (int i = 0; i < 8; i++)
+		board[i].resize(8);
+	}
+
+	// Initialize the tiles
+	for (int i = 0; i < 8; i++)
+		for (int j = 0; j < 8; j++)
+			board[i][j].state = 0;
+}
+
+Board::~Board()
+{
+	// Delete the board
+	board.clear();
+}
+
+void Board::initialize()
+{
+	// Initialise board array
+	for (int i = 0; i < 8; i++)
+		for (int j = 0; j < 8; j++)
 		{
-			board[i].resize(8);
+			if (i == 1 || i == 6)
+				board[i][j].state |= pieceFlags::pawn;
+			if (i == 0 || i == 7)
+			{
+				if (j == 0 || j == 7)
+					board[i][j].state |= pieceFlags::rook;
+				if (j == 1 || j == 6)
+					board[i][j].state |= pieceFlags::knight;
+				if (j == 2 || j == 5)
+					board[i][j].state |= pieceFlags::bishop;
+				if (j == 3)
+					board[i][j].state |= pieceFlags::queen;
+				if (j == 4)
+					board[i][j].state |= pieceFlags::king;
+			}
+			if (i == 6 || i == 7)
+				board[i][j].state |= pieceFlags::isWhitePiece;
 		}
+}
 
-		// Initialize the tiles
-		for (int i = 0; i < 8; i++)
-			for (int j = 0; j < 8; j++)
-				board[i][j].piece_color = 0;
-	}
-	~Board()
-	{
-		// Delete the board
-		board.clear();
-	}
-};
+std::vector<std::vector<tile_t>> &Board::getBoard()
+{
+	return board;
+}
+
+int Board::getRank()
+{
+	return rank;
+}
+
+int Board::getFile()
+{
+	return file;
+}
+
+void Board::setRank(int rank)
+{
+	this->rank = rank;
+}
+
+void Board::setFile(int file)
+{
+	this->file = file;
+}
