cb4dfaba0fa76f59eaaf7b2b1437f4fb81cf1d1b
CruzerNag <50835593+archishmannag@users.noreply.github.com>
2024-01-31 01:30:31 +0530

    The Game class now contains functions for all game components except the mover object which will move pieces.

--- include/game.h
+++ include/game.h
@@ -1,10 +1,36 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include <ncurses.h>
+#include <cstdlib>
+
 #include "board.h"
 #include "player.h"
 #include "clock.h"
-#include "move.h"
-#include "TerminalSizeWatcher.h"
+// #include "move.h"
+
+class Game
+{
+private:
+	Board *board;
+	Clock *clock;
+	Player *whitePlayer, *blackPlayer;
+	bool isWhiteTurn, isGameOver, undoMove;
+	int scoreUpdateReturned;
+
+public:
+	Game();
+	~Game();
+
+	void initialize();
+
+	void run();
+
+	void input();
+
+	void update();
+
+	void end();
+};
 
 #endif
--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,16 +1,107 @@
-/*
-	The main class for the game. This will consist of the board, pieces, players, clocks for the players and various function calls. The TUI will directly use this class' object to draw the board and pieces and to get input from the user.
-*/
-
 #include "../../include/game.h"
 
-class Game
+Game::Game()
 {
-private:
-	Board board;
-	WhitePlayer whitePlayer;
-	BlackPlayer blackPlayer;
-	Clock whiteClock, blackClock;
-	bool isWhiteTurn;
-	bool isGameOver;
-};
+	board = new Board();
+	clock = new Clock(10, 0, isWhiteTurn);
+	whitePlayer = new Player(true);
+	blackPlayer = new Player(false);
+	isWhiteTurn = true;
+	isGameOver = undoMove = false;
+
+	whitePlayer->setOpponent(blackPlayer);
+	blackPlayer->setOpponent(whitePlayer);
+
+	initialize();
+
+	run();
+}
+
+Game::~Game()
+{
+	delete board;
+	delete clock;
+	delete whitePlayer;
+	delete blackPlayer;
+}
+
+void Game::initialize()
+{
+	board->initialize();
+}
+
+void Game::run()
+{
+	clock->startClock();
+	while (!isGameOver)
+	{
+		input();
+		update();
+	}
+	end();
+}
+
+void Game::input()
+{
+	int ch;
+	ch = getch();
+	if (ch == 'q')
+	{
+		this->~Game();
+		exit(0);
+	}
+	else if (ch == KEY_UP)
+	{
+		if (board->getRank() > 0)
+			board->setRank(board->getRank() - 1);
+	}
+	else if (ch == KEY_DOWN)
+	{
+		if (board->getRank() < 7)
+			board->setRank(board->getRank() + 1);
+	}
+	else if (ch == KEY_LEFT)
+	{
+		if (board->getFile() > 0)
+			board->setFile(board->getFile() - 1);
+	}
+	else if (ch == KEY_RIGHT)
+	{
+		if (board->getFile() < 7)
+			board->setFile(board->getFile() + 1);
+	}
+	else if (ch == ' ')
+	{
+		scoreUpdateReturned = mover->move(board->getRank(), board->getFile());
+	}
+	else if (ch == 'u')
+	{
+		scoreUpdateReturned = mover->undo();
+		undoMove = true;
+	}
+}
+
+void Game::update()
+{
+	// Based upon the move, update the score of the player.
+	if (undoMove)
+	{
+		if (isWhiteTurn)
+			whitePlayer->setScore(whitePlayer->getScore() - scoreUpdateReturned);
+		else
+			blackPlayer->setScore(blackPlayer->getScore() - scoreUpdateReturned);
+		undoMove = false;
+	}
+	else
+	{
+		if (!isWhiteTurn)
+			whitePlayer->setScore(whitePlayer->getScore() + scoreUpdateReturned);
+		else
+			blackPlayer->setScore(blackPlayer->getScore() + scoreUpdateReturned);
+	}
+	scoreUpdateReturned = 0;
+}
+
+void Game::end()
+{
+}
