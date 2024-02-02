184a82da925f413c49a6a9e8194fa871e6ad50cf
CruzerNag <50835593+archishmannag@users.noreply.github.com>
2024-02-02 19:34:35 +0530

    Moved the terminal size watcher to the TUI folder.

---
+++ src/TUI/TerminalSizeWatcher.cpp
@@ -0,0 +1,20 @@
+#include "../../include/TerminalSizeWatcher.h"
+
+TerminalSizeWatcher::TerminalSizeWatcher()
+{
+	getTerminalSize();
+}
+
+void TerminalSizeWatcher::getTerminalSize()
+{
+	ioctl(0, TIOCGWINSZ, &ws);
+	cols = ws.ws_col;
+	lines = ws.ws_row;
+}
+
+void TerminalSizeWatcher::getDimensions(int *cols, int *lines)
+{
+	getTerminalSize();
+	*cols = this->cols;
+	*lines = this->lines;
+}
